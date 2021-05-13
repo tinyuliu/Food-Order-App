d1add43c36bd71c40195c48f7029fab4e075cacf
tinyuliu <tinyu1016@gmail.com>
2021-05-13 12:53:03 +0800

    146.Working on a MOre Complex Reducer Logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -23,8 +23,8 @@ const Cart = (props) => {
                     name={item.name} 
                     amount={item.amount} 
                     price={item.price}
-                    onRemove={cartItemRemoveHandler}
-                    onAdd={cartItemAddHandler} />
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null,item)} />
             ))}
         </ul>
     );
