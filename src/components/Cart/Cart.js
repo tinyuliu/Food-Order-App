ee0024b7991aa56e0071df9d17289d4b207c527c
tinyuliu <tinyu1016@gmail.com>
2021-06-30 15:42:57 +0800

    Fixed total amount value

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -39,7 +39,7 @@ const Cart = (props) => {
             <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>{totalAmount}</span>
-                <span>35.62</span>
+//                 <span>35.62</span>
             </div>
             <div className={classes.actions}>
                 <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
